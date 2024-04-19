e0852b693a8db4e8d515a35947111e91ce7a0e3f
Tarmo Pikaro <Tarmo.Pikaro@VisualComponents.com>
2024-04-19 08:31:58 +0300

    Update config file instead of generate it

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Windows;
-using System.Xml.Linq;
 
 namespace AppHost
 {
@@ -15,22 +14,23 @@ namespace AppHost
         static int Main(string[] args)
         {
             MethodInfo entry = null;
-            string mainExePath = ConfigurationManager.AppSettings["HostExe"];
-            if (String.IsNullOrEmpty(mainExePath) || !File.Exists(mainExePath))
+            string TargetHostExePath = ConfigurationManager.AppSettings["HostExe"];
+
+            if (String.IsNullOrEmpty(TargetHostExePath) || !File.Exists(TargetHostExePath))
             {
                 MessageBox.Show("Please reconfigure 'HostExe' value in AppHost.config file to point to valid executable");
                 return 0;
             }
-            string baseDir = Path.GetDirectoryName(mainExePath);
+            string baseDir = Path.GetDirectoryName(TargetHostExePath);
 
             try
             {
-                var asm = Assembly.LoadFrom(mainExePath);
+                var asm = Assembly.LoadFrom(TargetHostExePath);
 
                 var appType = asm.GetTypes().Where(x => typeof(Application).IsAssignableFrom(x)).FirstOrDefault();
                 if (appType == null)
                 {
-                    MessageBox.Show($"Executable '{Path.GetFileName(mainExePath)}' is not WPF application", "Info", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                    MessageBox.Show($"Executable '{Path.GetFileName(TargetHostExePath)}' is not WPF application", "Info", MessageBoxButton.OK, MessageBoxImage.Exclamation);
                     return 0;
                 }
 
@@ -39,7 +39,7 @@ namespace AppHost
                 var app = typeof(Application);
                 app.GetField("_resourceAssembly", BindingFlags.NonPublic | BindingFlags.Static).SetValue(null, asm);
 
-                var appConfig = Path.Combine(baseDir, mainExePath + ".config");
+                var appConfig = Path.Combine(baseDir, TargetHostExePath + ".config");
                 if(File.Exists(appConfig))
                 {
                     AppConfig.Change(appConfig);
